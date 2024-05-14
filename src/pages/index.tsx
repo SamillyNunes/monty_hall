aabb2359c09cc249f96788745a88e15bf71d8bd7
Samilly <samillynunes19@gmail.com>
2024-05-14 17:00:12 -0300

    ui: door implementation started

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import { Inter } from "next/font/google";
 
-import Gift from "../components/Gift";
+import Door from "../components/Door";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      <Gift />
+      <Door />
     </div>
   );
 }
