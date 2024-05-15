7967e7a0864f8bd7c3e0f715c7321c0d7aabe7b7
Samilly <samillynunes19@gmail.com>
2024-05-15 06:24:45 -0300

    selecting door feat

--- src/components/Door.tsx
+++ src/components/Door.tsx
@@ -2,17 +2,22 @@ import DoorModel from "../../model/door_model";
 import styles from "../styles/Door.module.css";
 
 interface DoorProps{
-    door: DoorModel
+    value: DoorModel,
+    onChange: (newDoor: DoorModel) => void,
 }
 
 export default function Door(props: DoorProps){
 
-    const { door } = props;
+    const door  = props.value;
 
     const selected = door.isSelected ? styles.selected : '';
 
+    function changeSelection(){
+        props.onChange(door.changeSelection());
+    }
+
     return (
-        <div className={styles.area} >
+        <div className={styles.area} onClick={changeSelection} >
             <div className={`${styles.structure} ${selected}`} >
                 <div className={styles.door} >
                     <div className={styles.number} > {door.number} </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,12 @@ import Door from "../components/Door";
 import DoorModel from "../../model/door_model";
 import { useState } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   const [d1, setD1] = useState(new DoorModel(1, false, true));
 
   return (
     <div style={{display: "flex"}} >
-      <Door door={d1} />
+      <Door value={d1} onChange={newDoor => setD1(newDoor)} />
     </div>
   );
 }
