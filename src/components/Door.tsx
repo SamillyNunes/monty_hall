b775736d8545f5047d8d6d1062c7ff2cdf767b5a
Samilly <samillynunes19@gmail.com>
2024-05-15 06:35:03 -0300

    feat: opening door

--- src/components/Door.tsx
+++ src/components/Door.tsx
@@ -1,3 +1,4 @@
+import { MouseEventHandler } from "react";
 import DoorModel from "../../model/door_model";
 import styles from "../styles/Door.module.css";
 
@@ -10,20 +11,35 @@ export default function Door(props: DoorProps){
 
     const door  = props.value;
 
-    const selected = door.isSelected ? styles.selected : '';
+    const selected = (door.isSelected && !door.isOpen) ? styles.selected : '';
 
     function changeSelection(){
         props.onChange(door.changeSelection());
     }
 
+    function openDoor(e:any){        
+        // Abaixo eh parada a propagacao do evento (onchange) para o component pai e evitar que este componente seja
+        // renderizado novamente, o que ocasionaria a impossibilidade de "abrir"
+        e.stopPropagation();
+        props.onChange(door.open());
+    }
+
+    function renderDoor(){
+        return (
+            <div className={styles.door} >
+                <div className={styles.number} > {door.number} </div>
+                <div className={styles.knob} onClick={openDoor} ></div>
+            </div>
+        );
+    }
+
     return (
         <div className={styles.area} onClick={changeSelection} >
             <div className={`${styles.structure} ${selected}`} >
-                <div className={styles.door} >
-                    <div className={styles.number} > {door.number} </div>
-                    <div className={styles.knob} ></div>
-                </div>
+                {door.isOpen ? false : renderDoor()}
+                
             </div>
+            
             <div className={styles.floor} ></div>
         </div>
     );
